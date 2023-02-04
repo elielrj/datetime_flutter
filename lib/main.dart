1ee9c365b8846afe0a3bc41e3e05e20e383d7ca6
Eliel de Souza Faria <elielrj@gmail.com>
2023-02-04 20:44:03 -0300

    exemplo de detetime

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,76 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_print
 
-void main() {
-  runApp(const MyApp());
+String tempoAgora() {
+  DateTime tempo = DateTime.now();
+  return "${tempo.hour}:${tempo.minute}:${tempo.second}:${tempo.microsecond}";
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main(List<String> args) {
+  print('Classe DateTime');
+  print('Copilando as ${tempoAgora()}');
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  var p1 = Pessoa('Eliel', 13, 3, 1987, horas: 10, minutos: 20);
+  print(p1);
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Pessoa {
+  String nome;
+  late DateTime _data;
+  late DateTime _nascimento;
+  final _agora = DateTime.now();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Pessoa(this.nome, int diaNascimento, int mesNascimento, int anoNascimento, {int horas = 0, int minutos = 0}) {
+    _nascimento = DateTime.utc(anoNascimento, mesNascimento, diaNascimento, horas, minutos);
+    print('$nome nascido em $diaNascimento/$mesNascimento/$anoNascimento ${diaSemana(_nascimento.weekday)} as $horas:$minutos ano ${bisexto(anoNascimento)}');
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    fecundacao();
+    tempo();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  fecundacao() {
+    int gestacaoMedia = 268;
+    _data = _nascimento.subtract(Duration(days: gestacaoMedia));
+
+    print('Data estimada da fecundação: ${_data.day}/${_data.month}/${_data.year}');
+  }
+
+  tempo() {
+    print('Tem ${idade().toStringAsFixed(0)} anos e está vivo a: '
+        '\n${_agora.difference(_nascimento).inDays} dias...'
+        '\n${_agora.difference(_nascimento).inHours} dias...'
+        '\n${_agora.difference(_nascimento).inMinutes} dias...'
+        '\n${_agora.difference(_nascimento).inSeconds} dias...');
+  }
+
+  String bisexto(int ano) => (ano % 4 == 0 || ano % 400 == 0 && ano % 100 != 0) ? 'é bisexto' : 'não é bisexto';
+
+  double idade() => _agora.difference(_nascimento).inDays / 360;
+
+  String diaSemana(int dia) {
+    switch (dia) {
+      case 1:
+        return 'segunda-feira';
+
+      case 2:
+        return 'terça-feira';
+
+      case 3:
+        return 'quarta-feira';
+
+      case 7:
+        return 'domingo';
+
+      case 4:
+        return 'quinta-feira';
+
+      case 5:
+        return 'sexta-feira';
+
+      case 6:
+        return 'sábado-feira';
+
+      default:
+        return 'erro';
+    }
   }
 }
